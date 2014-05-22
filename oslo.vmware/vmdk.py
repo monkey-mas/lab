62d769bb919247aaae07712dba0fd7efa5ac721e
Masaru Nomura <massa.nomura@gmail.com>
2014-05-22 15:33:47 +0100

    Add http support and exceptions
    Add http support for the following get methods so that VMDK files are
    handled not only locally but also remotely:
    *get_adapter_type
    *get_disk_type
    *get_virtual_size
    
    Also, add exceptions which is raised if expected property is not
    found in VMDK file.

--- oslo.vmware/vmdk.py
+++ oslo.vmware/vmdk.py
@@ -1,52 +1,108 @@
+from contextlib import closing
+from urllib2 import urlopen
+#from oslo.vmware import exceptions
+
 class API(object):
 
-	def get_adapter_type(self, vmdk):
-	    """Return adapater type of a vmdk file of type string
+    def get_adapter_type(self, vmdk):
+        """Return string of adapater type defined in a VMDK file
 
-	    :param vmdk: file path of vmdk file
-
-        Currently assuming format of vmdk is valid
+        Param vmdk is handled properly for both local files and http sources.
+        Currently assuming format of VMDK file is valid
         e.g) no missing fields
-	    """
-	    with open(vmdk, 'r') as f:
-	        for line in f:
-	            info = line.split()
 
-	            if info == []:
-	                continue
-	            if info[0] == 'ddb.adapterType':
-	                return info[2]
+        :param vmdk: file path of VMDK file
+        """
+        adapter_type = None
 
-	def get_disk_type(self, vmdk):
-	    """Return disk type of a vmdk file of type string
+        def extract_adapter_type(file):
+            for line in file:
+                info = line.split()
+                if info == []:
+                    continue
+                if info[0] == 'ddb.adapterType':
+                    #info format => ['ddb.adapterType','=',adapter_type]
+                    return info[2]
 
-	    :param vmdk: file path of vmdk file
+        if vmdk.startswith(('http:')):
+            with closing(urlopen(vmdk)) as page:
+                adapter_type = extract_adapter_type(page)
+        else:
+            with open(vmdk, 'r') as f:
+                adapter_type = extract_adapter_type(f)
 
-        Currently assuming format of vmdk is valid
+        if not adapter_type:
+            excep_msg = _("Could not extract adapter_type from VMDK file.")
+            LOG.error(excep_msg)
+            raise exceptions.VimException(excep_msg)
+
+        return adapter_type
+
+    def get_disk_type(self, vmdk):
+        """Return string of disk type defined in a VMDK file
+
+        Param vmdk is handled properly for both local files and http sources.
+        Currently assuming format of VMDK file is valid
         e.g) no missing fields
-	    """
-	    with open(vmdk, 'r') as f:
-	        for line in f:
-	            info = line.split('=')
 
-	            if info == []:
-	                continue
-	            if info[0] == 'createType':
-	            	createType_without_newline = info[1][:-1]
-	                return createType_without_newline
+        :param vmdk: file path of VMDK file
+        """
+        disk_type = None
 
-	def get_virtual_size(self, vmdk):
-	    """Return virtual size of a vmdk file of type string
+        def extract_disk_type(file):
+            for line in file:
+                info = line.split('=')
 
-	    :param vmdk: file path of vmdk file
+                if info == []:
+                    continue
+                if info[0] == 'createType':
+                    #info format => ['createTyel', disk_type'\n']
+                    createType_without_newline = info[1][:-1]
+                    return createType_without_newline
 
-        Currently assuming format of vmdk is valid
+        if vmdk.startswith(('http:')):
+            with closing(urlopen(vmdk)) as page:
+                disk_type = extract_disk_type(page)
+        else:
+            with open(vmdk, 'r') as f:
+                disk_type = extract_disk_type(f)
+
+        if not disk_type:
+            excep_msg = _("Could not extract disk_type from VMDK file.")
+            LOG.error(excep_msg)
+            raise exceptions.VimException(excep_msg)
+
+        return disk_type
+
+    def get_virtual_size(self, vmdk):
+        """Return string of virtual size defined in a VMDK file
+
+        Param vmdk is handled properly for both local files and http sources.
+        Currently assuming format of VMDK file is valid
         e.g) no missing fields
-	    """
-	    with open(vmdk, 'r') as f:
-	        for line in f:
-	            if line == '# Extent description\n':
-	                # description format => '<accesstype>[space]<size>[space]' 
-	                description = f.next()
-	                size        = description.split()[1]
-	                return size
+
+        :param vmdk: file path of VMDK file
+        """
+        size = None
+
+        def extract_virtual_size(file):
+            for line in file:
+                if line == '# Extent description\n':
+                    # description format => '<accesstype>[space]<size>[space]' 
+                    description = f.next()
+                    size        = description.split()[1]
+                    return size
+
+        if vmdk.startswith(('http:')):
+            with closing(urlopen(vmdk)) as page:
+                size = extract_virtual_size(page)
+        else:
+            with open(vmdk, 'r') as f:
+                size = extract_virtual_size(f)
+
+        if not size:
+            excep_msg = _("Could not extract virtual size from VMDK file.")
+            LOG.error(excep_msg)
+            raise exceptions.VimException(excep_msg)
+
+        return size
